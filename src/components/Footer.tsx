78cfbf475fb61d53f3bda3baefa05cff609ad0b9
geekymon2 <geekymon2@gmail.com>
2025-07-06 18:12:59 +1000

    initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <h5>Copyright @GeekyMon2. All Rights Reserved</h5>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>This is my Header</h1>
+    </div>
+  );
+};
+
+export default Header;
